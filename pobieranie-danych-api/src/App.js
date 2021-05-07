25c31076c97f8e8cd02795172cb4f2bdab4a65d3
lukasz protosawicki <lukas.es.p@o2.pl>
2021-05-07 10:09:19 +0200

    add prestahsop

--- pobieranie-danych-api/src/App.js
+++ pobieranie-danych-api/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    text: "w tym roku",
+    error: "",
+  };
+
+  handleDateChange = (e) => {
+    // console.log("zmiana");
+    const value = this.refs.number.value;
+    console.log(value);
+  };
+
+  render() {
+    return (
+      <div>
+        <input onChange={this.handleDateChange} type="text" ref="number" />
+        <p>Odp: {this.state.text} </p>
+      </div>
+    );
+  }
 }
-
 export default App;
